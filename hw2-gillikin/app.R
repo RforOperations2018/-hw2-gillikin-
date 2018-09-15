717ffe0fd6e03a9d323d23ff9443ac22cc67842d
gillikin <bgilliki@andrew.cmu.edu>
2018-09-14 21:45:37 -0400

    It's reactive!
    I really don't know how, but it is...

--- hw2-gillikin/app.R
+++ hw2-gillikin/app.R
@@ -13,11 +13,11 @@ flightData <- read.csv("flights.csv")
 
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("PIT", 
+ui <- navbarPage("Flights from Pittsburgh International Airport, January - April 2018", 
                  tabPanel("Plot",
                           sidebarLayout(
                             sidebarPanel(
-                              selectInput("airline",
+                              selectInput("airlineSelect",
                                           "Airline: ",
                                           choices = sort(unique(flightData$airline)),
                                           multiple = TRUE,
@@ -38,20 +38,29 @@ ui <- navbarPage("PIT",
 
 # Define server logic
 server <- function(input, output) {
-  swInput <- reactive({
+
+  fdInput <- reactive({
     flights <- flightData
+  #    filter(number >= input$numberSelect[1] & number <= input$numberSelect[2])
+    
+    if (length(input$airlineSelect) > 0 ) {
+      flights <- subset(flights, airline %in% input$airlineSelect)
+    }
+    
+    return(flights)
   })
 
+
   output$plot <- renderPlotly({
-    dat <- swInput()
+    dat <- fdInput()
     ggplotly(
-      ggplot(data = dat, aes(x = airline, y = number, color = date)) + 
+      ggplot(data = dat, aes(x = airline, y = number, color = month)) + 
         geom_point() +
         guides(color = FALSE)
       , tooltip = "text")
   })
   output$table <- DT::renderDataTable({
-    subset(swInput(), select = c(airline, date, number))
+    subset(fdInput(), select = c(month, airline, destination, number))
   })
 }
 
